45a8d171318a04fc4b7341967ccc0ef99813c893
Carly Ciricillo <carlyciricillo.media@gmail.com>
2019-09-01 14:28:34 -0400

    completes assignment

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 
 $(function(){
 
+	// create a function that converts kelvin to farenheit
+	function getFarenheit(kelvin){
+
+		// first convert kelvin to celcius
+		const celcius = kelvin - 273
+
+		// then convert celcius to farenheit
+		const farenheit = celcius * (9/5) + 32
+
+		// return rounded down farenheit
+		return Math.floor(farenheit)
+
+	}
+
 	// create an event handler function for the .submit() event
 	$('#search').submit((event) => {
 
@@ -13,19 +27,17 @@ $(function(){
 		const query = $('#query').val().toLowerCase()
 		console.log(`search term: ${query}`)
 
-		// run the search function, created below, with the above variable as the argument
+		// pass variable into search function
 		search(query)
 
 	})
-
-	// create a function to display the HTTP response
 
 	// create a function to make the API request
 	function search(query) {
 
 		// hardcode from OWM
-	    const url = 'https://samples.openweathermap.org/data/2.5/weather'
-	    const apiKey = ''
+	    const url = 'https://api.openweathermap.org/data/2.5/weather'
+	    const apiKey = '157d449680e8124c5c5c3c4ceecae869'
 
 	    $.ajax({
 		    url: url,
@@ -37,14 +49,67 @@ $(function(){
 	    
 	    // execute this function if request is successful
 	    .done((response) => {
+
 	    	// console logging the response will help you know what to do with it
 		    console.log(response)
+
+		    // pass the response into displayResults()
+		    displayResults(response)
+
 	    })
 
 	    // execute this function if request fails
 	    .fail(() => {
-		    console.log('fail')
+
+		    console.log('error')
+
 	    })
+
+	}
+
+	// create a function to display the response data from .done()
+	function displayResults(weatherData){
+
+		// pass kelvin data to getFarenheit()
+		const currentTemp = getFarenheit(weatherData.main.temp)
+		console.log(`farenheit: ${currentTemp}`)
+
+		const maxTemp = getFarenheit(weatherData.main.temp_max)
+		console.log(`farenheit max: ${maxTemp}`)
+
+		const minTemp = getFarenheit(weatherData.main.temp_min)
+		console.log(`farenheit min: ${minTemp}`)
+
+		// format results into HTML table
+		$('#displayResults').html(
+			`<table>
+				<thead>
+					<tr>
+						<th>${weatherData.name}</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td id="resultsDescription">${weatherData.weather[0].description}</td>
+					</tr>
+					<tr>
+						<td class="resultsTemps">Right now: <span class="currentTemp">${currentTemp}&deg; F</span></td>
+					</tr>
+					<tr>
+						<td class="resultsTemps">Today's range: ${maxTemp} - ${minTemp}</td>
+					</tr>
+				</tbody>
+			</table>`
+		)
+
+		// style results
+		$('#displayResults').css('margin', '20px 0')
+
+		if (currentTemp < 40) {
+			$(".currentTemp").addClass("lowCurrentTemp")
+		} else if (currentTemp > 90) {
+			$(".currentTemp").addClass("highCurrentTemp")
+		}
 
 	}
 
